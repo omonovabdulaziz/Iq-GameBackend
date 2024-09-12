7e65372f509661932b3ac1067595dc73b4090e42
golanguzb71 <abdulaziomonov4@gmail.com>
2024-09-12 14:14:25 +0500

    Project set

---
+++ src/test/java/it/live/iqgame/IqgameApplicationTests.java
@@ -0,0 +1,13 @@
+package it.live.iqgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
